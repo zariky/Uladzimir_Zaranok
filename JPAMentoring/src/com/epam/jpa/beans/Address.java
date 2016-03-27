546c170dbe1cdf223a5585f1a38e29254b7bd8e7
Admin <Admin@SonyVAIO>
2016-03-27 18:18:22 +0300

    JPA 1st commit

---
+++ JPAMentoring/src/com/epam/jpa/beans/Address.java
@@ -0,0 +1,51 @@
+package com.epam.jpa.beans;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class Address {
+	private String country;
+	private String city;
+	private String street;
+	
+	public Address(String country, String city, String street) {
+		super();
+		this.country = country;
+		this.city = city;
+		this.street = street;
+	}
+
+	public Address() {
+		super();
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	@Override
+	public String toString() {
+		return "Address [country=" + country + ", city=" + city + ", street=" + street + "]";
+	}
+	
+}
